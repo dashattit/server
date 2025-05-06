03c1ff6c802eebea4468e4e110d71abc2d9e23ab
gr422_ghddvl <dashyla.2021@mail.ru>
2025-05-06 12:45:54 +0700

    modific register

--- public/index.php
+++ public/index.php
@@ -3,3 +3,13 @@
 declare(strict_types=1);
 //Включаем сессии на все страницы
 session_start();
+
+try {
+    //Создаем экземпляр приложения и запускаем его
+    $app = require_once __DIR__ . '/../core/bootstrap.php';
+    $app->run();
+} catch (\Throwable $exception) {
+    echo '<pre>';
+    print_r($exception);
+    echo '</pre>';
+}
